430ee8cc81f7221d2138c6218e506b79f3b3e6b6
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-06-29 16:20:24 +0500

    initial

--- app/src/main/java/com/example/paits/MainActivity.java
+++ app/src/main/java/com/example/paits/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.paits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    Runnable runnable;
+    ImageView img;
+    private Bundle savedInstanceState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.splashscreen_img_logo);
+        img.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dsp = new Intent(MainActivity.this,LoginScreen.class);
+                startActivity(dsp);
+                finish();
+            }
+        },4000);
     }
+
+
+
 }
