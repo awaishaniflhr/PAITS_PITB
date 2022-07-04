8cc450ada50d6be0aba86f3331ea22fe004655e6
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-07-04 10:27:14 +0500

    initial 2

--- app/src/main/java/com/example/paits/LoginScreen.java
+++ app/src/main/java/com/example/paits/LoginScreen.java
@@ -2,13 +2,39 @@ package com.example.paits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        loginbtn = (Button) findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewRegistrationPIATS();
+            }
+        });
+
+//        initializeViews();
+//        listeners();
     }
-}
+
+    private void openNewRegistrationPIATS() {
+        Intent intent = new Intent(this, NewRegistrationPIATS.class);
+        startActivity(intent);
+    }
+
+
+    }
+
+
+
---
+++ app/src/main/java/com/example/paits/Splash_PIATS.java
@@ -0,0 +1,14 @@
+package com.example.paits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_PIATS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_piats);
+    }
+}
